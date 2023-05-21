f9d7d74811c0cab2bcc410802c760b00ed656dcb
prakashgole <96618533+prakashgole@users.noreply.github.com>
2023-05-21 17:14:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+#This code creates a basic web server that listens on the port defined by the PORT environment variable
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
